61c2f6a3eee93c750d09b65a1fca67f12f79665c
Eleazarguitar18 <eleazarguitar18@gmail.com>
2026-05-27 00:38:17 -0400

    fix:sockets

--- src/modules/Inventario/Categorias/services/categoriasService.ts
+++ src/modules/Inventario/Categorias/services/categoriasService.ts
@@ -12,25 +12,34 @@ interface Categoria {
 }
 export const CategoriasService = {
   async getCategories() {
-    const response = await axios.get(`${API_BASE_URL}/categorias`, { headers: getHeaders() });
+    const response = await axios.get(`${API_BASE_URL}/inventario/categorias`, {
+      headers: getHeaders(),
+    });
     return response;
   },
   async getCategoryById(id: number) {
-    const response = await axios.get(`${API_BASE_URL}/categorias/${id}`, { headers: getHeaders() });
+    const response = await axios.get(
+      `${API_BASE_URL}/inventario/categorias/${id}`,
+      { headers: getHeaders() },
+    );
     return response;
   },
   async createCategory(data: Categoria) {
     const user: any = localStorage.getItem("user");
     data.id_user_create = JSON.parse(user).id;
     // console.log("DATOS: ", data);
-    const response = await axios.post(`${API_BASE_URL}/categorias`, data, {
-      headers: getHeaders(),
-    });
+    const response = await axios.post(
+      `${API_BASE_URL}/inventario/categorias`,
+      data,
+      {
+        headers: getHeaders(),
+      },
+    );
     return response;
   },
   async updateCategory(id: number, data: Categoria) {
     const response = await axios.patch(
-      `${API_BASE_URL}/categorias/${id}`,
+      `${API_BASE_URL}/inventario/categorias/${id}`,
       data,
       {
         headers: getHeaders(),
@@ -39,9 +48,12 @@ export const CategoriasService = {
     return response;
   },
   async deleteCategory(id: number) {
-    const response = await axios.delete(`${API_BASE_URL}/categorias/${id}`, {
-      headers: getHeaders(),
-    });
+    const response = await axios.delete(
+      `${API_BASE_URL}/inventario/categorias/${id}`,
+      {
+        headers: getHeaders(),
+      },
+    );
     return response;
   },
 };
